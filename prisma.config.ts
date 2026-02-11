556533942577fbacea1e8bae978ae561ab61dd47
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-02-11 14:48:09 +0000

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: env.optional('DATABASE_URL') || 'postgresql://placeholder:placeholder@localhost:5432/placeholder',
   },
 });
