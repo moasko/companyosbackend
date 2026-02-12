2fba196a7cf6c04ba201cf37f3370af3724f21ec
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-02-12 21:41:50 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,17 +20,17 @@ async function bootstrap() {
 
   // Setup Swagger
   const config = new DocumentBuilder()
-    .setTitle('ENEA Telecom API')
-    .setDescription('The ENEA Telecom ERP & CMS API description')
-    .setVersion('1.0')
+    .setTitle("ENEA Telecom API")
+    .setDescription("The ENEA Telecom ERP & CMS API description")
+    .setVersion("1.0")
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup("api/docs", app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(port, "0.0.0.0");
+  console.log(`Application is running on port: ${port}`);
   console.log(`Swagger documentation: http://localhost:${port}/api/docs`);
 }
 bootstrap();
