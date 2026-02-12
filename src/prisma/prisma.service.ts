f0b128cee9ac0ee16e29742a5f2aa6485ad8050b
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-02-12 22:14:32 +0000

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     const pool = new pg.Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: true,
+      ssl: false,
     });
     const adapter = new PrismaPg(pool);
     super({ adapter });
